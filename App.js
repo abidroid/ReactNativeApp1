5e2666ea216c873e97d8d5fcf1dadbbb19bb8292
Muhammad Abid <abidroid1@gmail.com>
2025-04-13 15:38:40 +0500

    Welcome component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
     >
 
       <HeaderComponent />
+      <Welcome />
 
 
     </View>
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,1 +1,36 @@
+import { View, Text } from 'react-native';
 
+function Welcome() {
+
+    return (
+
+        <View
+            style={{
+                flex: 0.8,
+               
+
+            }}
+        >
+            <Text
+                style={{
+                    color: 'white',
+                    textAlign: 'center',
+                    fontSize: 40,
+                    padding: 50,
+                }}
+            >Welcome to IntelliLogics</Text>
+            <View style={{height: 20}}></View>
+            <Text
+                style={{
+                    color: 'white',
+                    textAlign: 'center',
+                    fontSize: 30,
+                    padding: 20,
+                }}
+            >IntelliLogics is a free study site where you can learn Mobile and Web Development.</Text>
+
+        </View>
+    );
+};
+
+export default Welcome;
