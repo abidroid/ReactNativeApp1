2994e056bb882e3ec94b7d824036ae1e2f783eff
Muhammad Abid <abidroid1@gmail.com>
2025-04-13 21:22:18 +0500

    stylesheet api

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,33 +1,21 @@
 import { View, Text, ScrollView } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 function Welcome() {
 
     return (
 
         <View
-            style={{
-                flex: 0.92,
-            }}
+            style={welcomeStyles.container}
         >
             <ScrollView
-            indicatorStyle={'white'}
-            >
+                indicatorStyle={'white'}>
                 <Text
-                    style={{
-                        color: 'white',
-                        textAlign: 'center',
-                        fontSize: 40,
-                        padding: 50,
-                    }}
+                    style={welcomeStyles.headerText}
                 >Welcome to IntelliLogics</Text>
                 <View style={{ height: 20 }}></View>
                 <Text
-                    style={{
-                        color: 'white',
-                        textAlign: 'center',
-                        fontSize: 30,
-                        padding: 20,
-                    }}
+                    style={welcomeStyles.detailItemText}
                 >
                     IntelliLogics is a free study site where you can learn Mobile and Web Development.
                     IntelliLogics is a free study site where you can learn Mobile and Web Development.
@@ -43,3 +31,24 @@ function Welcome() {
 };
 
 export default Welcome;
+
+const welcomeStyles = StyleSheet.create({
+    container: {
+        flex: 0.92
+    },
+
+    headerText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 40,
+        padding: 50,
+    },
+
+    detailItemText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30,
+        padding: 20,
+    }
+}
+);
