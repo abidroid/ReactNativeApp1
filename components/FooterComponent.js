366cdeaa54a01d3b28a7e1c640568fe5ae411b55
Muhammad Abid <abidroid1@gmail.com>
2025-04-13 12:19:10 +0500

    footer

---
+++ components/FooterComponent.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+function FooterComponent() {
+
+    return (
+
+        <View style={{backgroundColor: 'yellow', padding: 8}}>
+            <Text>All rights reserved by IntelliLogics 2025</Text>
+        </View>
+    );
+}
+
+export default FooterComponent;
