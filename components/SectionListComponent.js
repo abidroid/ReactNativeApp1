4a97b7227c0812311e174ba7f349685c279509d1
Muhammad Abid <abidroid1@gmail.com>
2025-04-15 09:45:47 +0500

    fixes

--- components/SectionListComponent.js
+++ components/SectionListComponent.js
@@ -36,29 +36,29 @@ const menuItemsToDisplay = [
 
 
 
-    const Item = ({item}) => (
-        <View>
-            <Text>{item.name}</Text>
-        </View>
-    );
+const Item = ({ item }) => (
+    <View style={styles.itemRow}>
+        <Text style={styles.itemText}>{item}</Text>
+    </View>
+);
 
 const SectionListComponent = () => {
 
-    const renderItem = ({item}) => <Item item={item}/>
-    const renderSectionHeader = ({section: {title}}) => (
-        <Text>{title}</Text>
-    ); 
+    const renderItem = ({ item }) => <Item item={item} />
+    const renderSectionHeader = ({ section: { title } }) => (
+        <Text style={styles.sectionHeaderText}>{title}</Text>
+    );
 
     return (
 
-        <View>
+        <View style={styles.outerContainer}>
             <SectionList
                 keyExtractor={(item, index) => item + index}
                 sections={menuItemsToDisplay}
-                renderSectionHeader={ renderSectionHeader}
-                renderItem={ renderItem }
-                // ItemSeparatorComponent={ }
-                // ListFooterComponent={ }
+                renderSectionHeader={renderSectionHeader}
+                renderItem={renderItem}
+            // ItemSeparatorComponent={ }
+            // ListFooterComponent={ }
             />
         </View>
     );
@@ -66,3 +66,31 @@ const SectionListComponent = () => {
 }
 
 export default SectionListComponent;
+
+const styles = StyleSheet.create({
+
+    outerContainer: {
+        flex: 0.8,
+    },
+
+    sectionHeader: {
+        backgroundColor: 'blue',
+    },
+    sectionHeaderText: {
+        backgroundColor: '#fbdabb',
+        color: '#333333',
+        fontSize: 34,
+        flexWrap: 'wrap',
+        textAlign: 'center',
+    },
+
+    itemRow: {
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        backgroundColor: '#333333',
+      },
+    itemText: {
+        color: '#F4CE14',
+        fontSize: 32,
+    },
+});
