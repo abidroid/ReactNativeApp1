6576a51cd6949fe79f4ca45cf169d4598571796c
Muhammad Abid <abidroid1@gmail.com>
2025-04-16 21:01:56 +1000

    Pressable

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,34 +1,51 @@
 
 import React from 'react';
-import { ScrollView, Text, StyleSheet, TextInput } from 'react-native';
+import { ScrollView, Text, StyleSheet, TextInput, Pressable, Alert } from 'react-native';
 
 const LoginPage = () => {
 
     const [email, onChangeEmail] = React.useState('');
     const [password, onChangePassword] = React.useState('');
 
+    const [loggedIn, setLoggIn] = React.useState(false);
     return (
         <ScrollView style={styles.container}
-        keyboardDismissMode='on-drag'
+            keyboardDismissMode='on-drag'
         >
             <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-            <Text style={styles.regularText}>Login to continue </Text>
 
-            <TextInput
+
+            <Text style={styles.regularText}>
+
+                {loggedIn ? 'You are logged in' : 'Login to continue'}
+            </Text>
+
+            {!loggedIn && <TextInput
                 style={styles.input}
                 placeholder='email'
                 onChangeText={onChangeEmail}
                 keyboardType='email-address'
                 value={email}
-            />
+            />}
 
-            <TextInput
+
+            {!loggedIn && <TextInput
                 style={styles.input}
                 placeholder='password'
                 onChangeText={onChangePassword}
                 secureTextEntry={true}
                 value={password}
-            />
+            />}
+
+            <Pressable style={styles.button}
+                onPress={() => {
+                    setLoggIn(!loggedIn);
+                }}
+            >
+                <Text style={styles.buttonText}>
+                    {loggedIn ? 'Logout' : 'Login'}
+                </Text>
+            </Pressable>
         </ScrollView>
     );
 }
@@ -55,6 +72,22 @@ const styles = StyleSheet.create({
         height: 40,
         padding: 12,
         margin: 16,
-    }
+    },
+
+    button: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 40,
+        backgroundColor: '#EDEFEE',
+        borderColor: '#EDEFEE',
+        borderWidth: 2,
+        borderRadius: 48
+    },
+    buttonText: {
+        color: '#333333',
+        textAlign: 'center',
+        fontSize: 32,
+    },
 });
 export default LoginPage;
